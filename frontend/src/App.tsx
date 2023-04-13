c46e7360018a1e0b43fbc7f99f4ce6fd4d651fe2
skandertebo <alexandertebourb@gmail.com>
2023-04-13 13:55:21 +0100

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Button } from '@material-tailwind/react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <Button className="bg-red-500">Hello</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert';
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), mkcert()],
+
+  server: {
+    https: true,
+    cors: true,
+    port: 3000,
+    host: '0.0.0.0'
+  }
+});
