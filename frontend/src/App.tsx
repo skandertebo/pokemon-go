8808180955906f4ed9ac6f3dc41cde75a10f69d3
melek <mbensnoussi@gmail.com>
2023-04-20 23:08:24 +0100

    capturePage PokemonPage v1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,14 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
-import { Button } from '@material-tailwind/react';
+import CapturePage from './pages/CapturePage';
+import PokemonPage from './pages/PokemonPage';
+import pokemons from "./assets/pokemonsdata";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <Button className="bg-red-500">Hello</Button>
+      <PokemonPage pokemon={pokemons[0]} />
     </div>
   );
 }
