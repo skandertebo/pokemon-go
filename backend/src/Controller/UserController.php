07e635e4fafa1a0334edb60f99b2a35918a6d182
Yasmine Kharrat <yasminekharrat1@gmail.com>
2023-04-22 19:11:47 +0100

    userController

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Service\UserService;
 class UserController extends AbstractController
 {
 
