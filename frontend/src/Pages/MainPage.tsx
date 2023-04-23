978c95609758bcec914a95a33159705dd363d98c
skandertebo <alexandertebourb@gmail.com>
2023-04-23 02:16:43 +0100

    added pokemon displaying on map, added 3d world to capture

--- frontend/src/Pages/MainPage.tsx
+++ frontend/src/Pages/MainPage.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import WebMap from '../components/Map';
 import CaptureButton from '../components/CaptureButton';
 import DebugModal from '../components/DebugModal';
 import { useAppContext } from '../context/AppContext';
+import CaptureCam from '../components/CaptureCam';
+import { Spawn } from '../types/Spawn';
+import { spawns } from '../assets/fakeData.ts/spawns';
+
 declare global {
   interface Window {
     initMap: () => void;
@@ -11,19 +15,20 @@ declare global {
 
 const MainPage: React.FC = () => {
   const { geoLocationPosition } = useAppContext();
+  const [isCapturing, setIsCapturing] = useState<Spawn | null>(null);
 
   return (
     <div className='w-screen h-screen sm:w-full flex flex-col gap-8 items-center'>
-      {/*       <DebugModal
-        data={{
-          lat: geoLocationPosition?.coords?.latitude,
-          long: geoLocationPosition?.coords?.longitude
-        }}
-      /> */}
       <div className='h-[60vh] w-full shadow-md rounde-md'>
         <WebMap />
       </div>
-      <CaptureButton />
+      {isCapturing && (
+        <CaptureCam
+          spawn={isCapturing}
+          captureAction={() => setIsCapturing(null)}
+        />
+      )}
+      <CaptureButton onClick={() => setIsCapturing(spawns[0])} />
     </div>
   );
 };
