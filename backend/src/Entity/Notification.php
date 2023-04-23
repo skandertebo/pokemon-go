40a0d7a5dc5e7591eddc330c3fb3a14155cf6b72
skandertebo <alexandertebourb@gmail.com>
2023-04-23 14:58:29 +0100

    tweaks to notification columns and added functions to repository

--- backend/src/Entity/Notification.php
+++ backend/src/Entity/Notification.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\NotificationRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use JsonSerializable;
@@ -19,9 +20,22 @@ class Notification implements JsonSerializable
     #[ORM\Column(length: 255)]
     private ?string $contenu = null;
     
+    #[ORM\Column(type: 'datetime')]
+    private ?\DateTimeInterface $date = null;
+
+    // for reasons of optimization we could make the notification concern all users or only a specific group of users that share a property without having to make many UserNotification entities
+    // value '*' means all users
+    #[ORM\Column(length:10)]
+    private ?string $concerne = null;
     
+
     #[ORM\OneToMany(mappedBy: 'userId', targetEntity: UserNotification::class, orphanRemoval: true)]
     private Collection $userNotifications;
+
+    public function __construct()
+    {
+        $this->userNotifications = new ArrayCollection();
+    }
 
 
     public function getId(): ?int
@@ -79,5 +93,45 @@ class Notification implements JsonSerializable
             'id' => $this->getId(),
             'contenu' => $this->getContenu(),
         ];
+    }
+
+    /**
+     * Get the value of concerne
+     */ 
+    public function getConcerne()
+    {
+        return $this->concerne;
+    }
+
+    /**
+     * Set the value of concerne
+     *
+     * @return  self
+     */ 
+    public function setConcerne($concerne)
+    {
+        $this->concerne = $concerne;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of date
+     */ 
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set the value of date
+     *
+     * @return  self
+     */ 
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
     }
 }
