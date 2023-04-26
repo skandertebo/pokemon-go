eff982df04a0a35267ee62f3713f31a319b98675
amina ghannem <amynaghannem@gmail.com>
2023-04-26 21:57:19 +0100

    updates

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,7 +2,8 @@ import LoginComp from '../components/LoginComp';
 
 function Login() {
   return (
-    <div className="flex bg-[url('/images/login.jpg')] md:bg-[url('https://wallpaperaccess.com/full/1360529.jpg')] h-screen md:items-center lg:items-end md:shrink-0 bg-cover md:justify-end">
+    <div className="flex bg-[url('/images/login.jpg')] md:bg-[url('https://wallpaperaccess.com/full/1360529.jpg')] 
+    h-screen md:items-center lg:items-end md:shrink-0 bg-cover md:justify-end">
       <LoginComp />
     </div>
   );
