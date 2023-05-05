4aff7d383baf91661212f5512070473dee549ca2
amina ghannem <amynaghannem@gmail.com>
2023-05-05 12:16:58 +0100

    fix the name of the function

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,6 +1,6 @@
 import RegisterComp from '../components/RegisterComp';
 
-function Login() {
+function Register() {
     return (
         <div className="flex bg-[url('../../public/images/regist.png')] md:bg-[url('https://wallpaperaccess.com/full/7289617.jpg')] bg-center md:bg-left-top h-screen md:items-center lg:items-end  md:shrink-0 bg-cover md:justify-end">
             <RegisterComp/>
@@ -8,4 +8,4 @@ function Login() {
     );
 }
 
-export default Login;
+export default Register;
