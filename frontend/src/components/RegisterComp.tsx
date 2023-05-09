eeb8dcad152f7f86bfa2bb6948ced97066d5dd55
skandertebo <alexandertebourb@gmail.com>
2023-05-09 16:17:10 +0100

    tweaks to login and register components

--- frontend/src/components/RegisterComp.tsx
+++ frontend/src/components/RegisterComp.tsx
@@ -4,7 +4,7 @@ import { registerUser } from '../apiCalls/register';
 import { useAuthContext } from '../context/AuthContext';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { RegisterBody } from '../types/RegisterBody';
-import { VscEye,VscEyeClosed} from 'react-icons/vsc';
+import { VscEye, VscEyeClosed } from 'react-icons/vsc';
 import { useAppContext } from '../context/AppContext';
 
 function RegisterComp() {
@@ -12,7 +12,7 @@ function RegisterComp() {
   const [type, setType] = useState<string>('password');
   const { setToken, token } = useAuthContext()!;
   const [error, setError] = useState<string>('');
-  const {enableWaiting, disableWaiting} = useAppContext();
+  const { enableWaiting, disableWaiting } = useAppContext();
   const [registerData, setRegisterData] = useState<RegisterBody>({
     playerTag: '',
     email: '',
@@ -60,83 +60,97 @@ function RegisterComp() {
 
   return (
     <>
-    <form
-      className='text-center max-w-screen-md w-full m-auto p-5 mb-[210px]
+      <form
+        className='text-center max-w-screen-md w-full m-auto p-5 mb-[50%]
     md:p-1/5 md:mb-[100px] md:mr-[100px] md:w-[400px] 
     lg:mr-[100px] lg:mb-[350px] lg:w-[600px] '
-      onSubmit={handleSubmit}
-    >
-      <div className='m-2'>
-        <input
-          className='w-full h-12 px-2.5 rounded-md border-solid border-2 lg:w-[400px]'
-          type='text'
-          id='playerTag'
-          name='playerTag'
-          value={registerData.playerTag}
-          onChange={handleChange}
-          placeholder='Enter your playerTag'
-          required
-        />
-      </div>
-      <div className='m-2'>
-        <input
-          className='w-full h-12 px-2.5 rounded-md border-solid border-2 lg:w-[400px]'
-          type='email'
-          id='email'
-          name='email'
-          value={registerData.email}
-          onChange={handleChange}
-          placeholder='Enter your Email'
-          required
-        />
-      </div>
-      <div className='m-2 relative'>
-        <input
-          className='w-full h-12 px-2.5 rounded-md border-solid border-2 lg:w-[400px]'
-          type={type}
-          id='password'
-          name='password'
-          value={registerData.password}
-          onChange={handleChange}
-          placeholder='Enter Your Password'
-          required
-        />
-        <VscEyeClosed className={openEye ? 'absolute text-2xl text-primary right-2 top-3 cursor-pointer' : 'hidden'} onClick={()=>onClickEye()}/>
-        <VscEye className={openEye ? 'hidden' :'absolute text-2xl text-primary right-2 top-3 cursor-pointer' } onClick={()=>onClickEye()}/>
-      </div>
-      <div className='m-2'>
-        <Button
-          className='bg-primary  w-3/4 h-12 rounded-md font-bold text-secondary md:w-[300px] lg:w-[300px]'
-          type='submit'
-        >
-          Register
-        </Button>
-      </div>
-      <h2
-        className='text-base text-primary md:text-primary rounded-md mx-auto bg-gray-100 rounded-md bg-opacity-60 p-2'
-         style={{ width: 'fit-content' }}
+        onSubmit={handleSubmit}
       >
-        Already have an account?
-        <Link
-          className='text-base font-bold text-primary italic hover:not-italic md:text-secondary lg:text-primary '
-          to='/login'
-        >
-          {' '}
-          Login
-        </Link>
-      </h2>
-        </form>
-      <div className='absolute bottom-3 left-1/2 transform -translate-x-1/2 w-full '>
-      {error && (
-        <div
-          className='p-2 rounded-md mt-4 bg-red-500 text-white text-center mx-auto'
+        <div className='m-2'>
+          <input
+            className='w-full h-12 px-2.5 rounded-md border-solid border-2 lg:w-[400px]'
+            type='text'
+            id='playerTag'
+            name='playerTag'
+            value={registerData.playerTag}
+            onChange={handleChange}
+            placeholder='Enter your playerTag'
+            required
+          />
+        </div>
+        <div className='m-2'>
+          <input
+            className='w-full h-12 px-2.5 rounded-md border-solid border-2 lg:w-[400px]'
+            type='email'
+            id='email'
+            name='email'
+            value={registerData.email}
+            onChange={handleChange}
+            placeholder='Enter your Email'
+            required
+          />
+        </div>
+        <div className='m-2 relative'>
+          <input
+            className='w-full h-12 px-2.5 rounded-md border-solid border-2 lg:w-[400px]'
+            type={type}
+            id='password'
+            name='password'
+            value={registerData.password}
+            onChange={handleChange}
+            placeholder='Enter Your Password'
+            required
+          />
+          <VscEyeClosed
+            className={
+              openEye
+                ? 'absolute text-2xl text-primary right-2 top-3 cursor-pointer'
+                : 'hidden'
+            }
+            onClick={() => onClickEye()}
+          />
+          <VscEye
+            className={
+              openEye
+                ? 'hidden'
+                : 'absolute text-2xl text-primary right-2 top-3 cursor-pointer'
+            }
+            onClick={() => onClickEye()}
+          />
+        </div>
+        <div className='m-2'>
+          <Button
+            className='bg-primary  w-3/4 h-12 rounded-md font-bold text-secondary md:w-[300px] lg:w-[300px]'
+            type='submit'
+          >
+            Register
+          </Button>
+        </div>
+        <h2
+          className='text-base text-primary md:text-primary mx-auto bg-gray-100 rounded-md bg-opacity-70 p-2'
           style={{ width: 'fit-content' }}
         >
-          {error}
-        </div>
-      )}
+          Already have an account?
+          <Link
+            className='text-base font-bold text-primary italic hover:not-italic'
+            to='/login'
+          >
+            {' '}
+            Login
+          </Link>
+        </h2>
+      </form>
+      <div className='absolute bottom-3 left-1/2 transform -translate-x-1/2 w-full '>
+        {error && (
+          <div
+            className='p-2 rounded-md mt-4 bg-red-500 text-white text-center mx-auto'
+            style={{ width: 'fit-content' }}
+          >
+            {error}
+          </div>
+        )}
       </div>
-      </>
+    </>
   );
 }
 
